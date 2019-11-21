fd681390bf8e0d533760df9ac34f8cd16bf41ba5
cesar mejia <tu@ejemplo.com>
2019-11-21 05:01:10 +0100

    se agrego base.thml y editar.html

--- menus/views.py
+++ menus/views.py
@@ -9,7 +9,7 @@ def menu_nueva(request):
         formulario = MenuForm(request.POST)
         if formulario.is_valid():
              menu = Menu.objects.create(nombre=formulario.cleaned_data['nombre'], total = formulario.cleaned_data['total'])
-             for plato_id in request.POST.getlist('platos'):
+             for plato_id in request.POST.getlist('platillos'):
                  comida = Comida(plato_id=plato_id, menu_id = menu.id)
                  comida.save()
              messages.add_message(request, messages.SUCCESS, 'Guardado Exitosamente')
